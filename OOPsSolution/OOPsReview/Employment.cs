86fbb6d385bbb6551a040594843c77fdc8dd314d
PotatoWedges30 <96024767+PotatoWedges30@users.noreply.github.com>
2023-05-15 18:16:18 -0600

    complete properties review, demonstrate setting up a common utility static class with overloaded methods

--- OOPsSolution/OOPsReview/Employment.cs
+++ OOPsSolution/OOPsReview/Employment.cs
@@ -12,8 +12,8 @@ namespace OOPsReview
         /// data members for class
         /// </summary>
         private SupervisoryLevel _Level;
-        private string _Title;
-        private double _Years;
+        private string _Title;     // default of string is null
+        private double _Years;     // default of numeric is 0
 
         /// <summary>
         /// Properties
@@ -36,6 +36,7 @@ namespace OOPsReview
 
 
         /// <summary>
+        ///  Fully implemented property : Title, Level
         /// Property: Title
         /// validation: there must be a character string
         /// a property will always have an accessor (get)
@@ -48,6 +49,7 @@ namespace OOPsReview
         ///       public: accessable by outside users of the class
         ///       private:accessable ONLY within the class
         /// a property DOSE NOT have ANY declare incoming parameter list!!!!!!
+        /// 
         /// </summary>
         public string Title
         {
@@ -58,6 +60,7 @@ namespace OOPsReview
             // referred to as the mutator
             // it is within the set that the validation of the data
             // is done to determine if the data is acceptable
+            // by default set is public
             set 
             { 
                 if (string.IsNullOrWhiteSpace(value))
@@ -71,7 +74,74 @@ namespace OOPsReview
             }
         }
 
-       
-       
+        
+        public SupervisoryLevel Level {
+            get {   return _Level; }
+
+           private  set
+           {
+                /** 
+                 *  a private set means that the property can only be set
+                 *  by code within the class definition (outside class can not do this)
+                 * 
+                 *  An advantage of doing this is increaseing security on the 
+                 *   field as any change is under the control of the class definition
+                 *  
+                 *   validate the value given as an enum is actually defined
+                 *   
+                 *  a user of this class could send in an integer value that was
+                 *   type casted as this enum datatype BUT have a non-defined value
+                 *   
+                 *  To test for defined enum value use Enum.IsDefined(typeof(xxxx), value)
+                 *   where xxxx is the name of the enum datatype
+                 */
+                if (!Enum.IsDefined(typeof(SupervisoryLevel), value))
+                {
+                    throw new ArgumentException($"Supervisory level is invalid: {value} ");
+                }
+                _Level = value;
+
+           }
+        
+        }
+
+
+        // validate the years of service in the employee position as a positive value.
+
+        public double Years
+        {
+            get { return _Years; } // used on the right side of an assignment statement or in an expression
+
+            set                   // used on the left side of an assignment statement
+            {
+               // if (value < 0 ) // using a Utilities generic method to do this test
+               if(!Utilities.IsZeroOrPositive(value))
+                { 
+                    throw new ArgumentOutOfRangeException("value");
+                }
+                _Years = value;
+            }
+        }
+
+
+        /**
+         *  This property is an example of an auto implemented property
+         *  
+         *  There is no validation within the property
+         *  There is NO private data member for this property
+         *  The system will generate an internal storage area for the data
+         *     and handle the setting and getting from that storage area
+         *     
+         *  The private set means that the property will only be able to be
+         *      set via a constructor or method.
+         *      
+         *  auto implemented properties can have either a public or private set
+         *  using a public or private set is a design decision
+         */
+        public DateTime StartDate { get; private set; }
+
+
+
+
     }
 }
