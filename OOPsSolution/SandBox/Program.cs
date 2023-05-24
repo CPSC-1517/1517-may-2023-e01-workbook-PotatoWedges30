bb869f9997676f65d28f34ba074c53c8d7c02f91
PotatoWedges30 <96024767+PotatoWedges30@users.noreply.github.com>
2023-05-24 17:46:39 -0600

    commit introduction to record

---
+++ OOPsSolution/SandBox/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using OOPsReview;
+
+
+
+Residence myHome = new Residence(123, "Maple St.", "Edmonton", "AB", "T6Y7U8");
+Console.WriteLine(myHome.ToString());
+
+/** 
+ * Can I change a value in the record instance? NO
+ * myHome.Number = 321;
+ */
+
+// to alter a value in the record instance you MUST create a neew instance
+
+ myHome = new Residence(321, "Maple St.", "Edmonton", "AB", "T6Y7U8");
+Console.WriteLine(myHome.ToString());
