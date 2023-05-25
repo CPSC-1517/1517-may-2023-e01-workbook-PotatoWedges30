42a98d60721c9b33f6fa4bcc951bca1aa814c60f
PotatoWedges30 <96024767+PotatoWedges30@users.noreply.github.com>
2023-05-24 18:19:10 -0600

    sample of refactoring

--- OOPsSolution/SandBox/Program.cs
+++ OOPsSolution/SandBox/Program.cs
@@ -15,3 +15,59 @@ Console.WriteLine(myHome.ToString());
 
  myHome = new Residence(321, "Maple St.", "Edmonton", "AB", "T6Y7U8");
 Console.WriteLine(myHome.ToString());
+
+// Example of refactoring
+// Refactoring is the process of restructing code, while not 
+// changing the original functionality. The goal of refactoring
+// is to improve internal code by making small changes without altering
+// the codes external behaviour
+
+// orignal code
+bool flag = false;
+
+if (myHome.Province.ToLower()=="ab")
+{
+    flag = true;    
+}
+
+if (myHome.Province.ToLower()=="bc")
+{
+    flag = true;
+}
+
+if (myHome.Province.ToLower()=="sk")
+{
+    flag = true;
+}
+
+if (myHome.Province.ToLower()=="mn")
+{
+    flag = true;
+}
+
+
+if (myHome.Province.ToLower() == "ab" || myHome.Province.ToLower() == "bc" || myHome.Province.ToLower() == "sk" || myHome.Province.ToLower() == "mn")
+{
+    flag = true;
+}
+
+
+
+//refactor using a switch statement
+
+switch (myHome.Province.ToLower())
+{
+    case "ab":
+    case "bc":
+    case "sk":
+    case "mn":
+        {
+            flag = true;
+            break;
+        }
+    default:
+        {
+            flag = true;
+            break;
+        }
+}
